4992d903e8d2eb80c8bae8b40d46249465fc4301
Omarree <Omarree.Kimbrough@digipen.edu>
2019-06-27 16:26:24 -0700

    hfdgfhfjhvf

---
+++ GroupTest/Assets/follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Vector3 destination = transform.position + new Vector3(0, 0, -10);
+        if (cam)
+            cam.transform.position = Vector3.Lerp(cam.transform.position, destination, 0.9f);
+            
+    }
+}
